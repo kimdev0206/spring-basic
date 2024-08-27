9e9bba36a425875ca4e8e7326147631d5a2a575d
Yongki Kim <kimdev0206@gmail.com>
2024-08-27 17:51:10 +0900

    add: 애플리케이션

---
+++ src/test/java/spring_basic/spring_basic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_basic.spring_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
